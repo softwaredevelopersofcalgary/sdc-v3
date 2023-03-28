800c3a78b191c9197f8a9d3b1e3b13f7bba60888
Forest Park <foresthpark@gmail.com>
2023-03-27 19:44:07 -0600

    Add .env.prod to .gitignore and update UserForm title input styling in UserForm component

--- src/components/UserForm/UserForm.tsx
+++ src/components/UserForm/UserForm.tsx
@@ -57,7 +57,7 @@ export default function UserForm({ user, onSubmit, setIsOpen }: UserFormProps) {
             {...register("title")}
             id="title"
             placeholder={"Chief Fun Officer"}
-            className="block w-full max-w-lg rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:max-w-xs sm:text-sm sm:leading-6"
+            className="block w-full max-w-lg rounded-md border-0 p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:max-w-xs sm:text-sm sm:leading-6"
           />
         </div>
       </div>
