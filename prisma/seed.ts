ab23556632a1bfada179825dbbf95920f22620de
Simon <29654835+absolutelysimon@users.noreply.github.com>
2023-11-25 15:01:43 -0700

    Finished the seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,4 +3,32 @@ import { data as tech_list } from "../src/helpers/tech.data";
 
 const prisma = new PrismaClient();
 
-async function main() {}
+async function main() {
+  for (let i = 0; i < tech_list.length; i++) {
+    const new_tech = tech_list[i];
+
+    const existingRecord = await prisma.masterTech.findFirst({
+      where: { slug: new_tech ? new_tech.slug : "" },
+    });
+
+    if (!existingRecord) {
+      const add_tech = await prisma.masterTech.create({
+        data: {
+          slug: new_tech ? new_tech.slug : "",
+          label: new_tech ? new_tech.label : "",
+          imgUrl: new_tech ? new_tech.imgUrl : "",
+        },
+      });
+    }
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
