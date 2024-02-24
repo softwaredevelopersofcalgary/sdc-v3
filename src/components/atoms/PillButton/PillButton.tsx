a3a4957d6961322a1af573d6eaeaf925b6218743
NHanzhuha <NHanzhuha@healthprecision.com>
2024-02-24 13:05:44 -0700

    Fixes Added tooltip for "Join Project" button when you have already joined to another project

--- src/components/atoms/PillButton/PillButton.tsx
+++ src/components/atoms/PillButton/PillButton.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 interface PillButtonProps {
   label: string;
@@ -15,7 +15,11 @@ export default function PillButton({
   isUserPartOfAnyProject,
   isLoading,
 }: PillButtonProps) {
+
+  const [showTooltip, setShowTooltip] = useState(false);
+
   return (
+    <div className="relative">
     <button
       title={
         isMember
@@ -24,7 +28,9 @@ export default function PillButton({
           ? "You're already a member of a different project"
           : "Join Project"
       }
-      disabled={isMember ? false : isUserPartOfAnyProject ? true : false}
+      onMouseOver={() => setShowTooltip(true)}
+      onMouseLeave={() => setShowTooltip(false)}
+      disabled={isMember ? false : !!isUserPartOfAnyProject}
       className={`
       ${
         isLoading
@@ -32,7 +38,7 @@ export default function PillButton({
           : isMember
           ? "cursor-pointer border-gray-500 bg-gray-100 text-gray-800"
           : isUserPartOfAnyProject
-          ? "cursor-not-allowed border-red-500 bg-red-100 text-red-800"
+          ? "cursor-not-allowed border-red-500 bg-red-100 text-red-800 disabled:bg-red-100"
           : "cursor-pointer border-gray-500 bg-gray-100 text-gray-800"
       }
       inline-flex items-center rounded-full border-2 px-5 py-2 text-xs font-bold `}
@@ -40,5 +46,12 @@ export default function PillButton({
     >
       {label}
     </button>
+
+      {!isMember && isUserPartOfAnyProject && showTooltip &&
+        <div className="absolute bg-gray-400 text-white p-2 rounded mt-2 bottom-0 right-0 w-36">
+          {"You're already a member of a different project"}
+        </div>
+      }
+    </div>
   );
 }
