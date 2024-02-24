646d8df39fa68cb67d94dde8184acbc31c12db66
Simon <29654835+absolutelysimon@users.noreply.github.com>
2024-02-23 23:55:26 -0700

    Trying a fix for this deployment error

--- src/components/EventDetailHeader/EventDetailHeader.tsx
+++ src/components/EventDetailHeader/EventDetailHeader.tsx
@@ -81,7 +81,7 @@ export default function EventDetailHeader({
               <button
                 type="button"
                 className="inline-flex items-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
-                onClick={() => signIn()}
+                onClick={() => void signIn()}
               >
                 Attend Event
               </button>
