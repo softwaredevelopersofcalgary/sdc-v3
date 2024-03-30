db1eac1e10e9d088d11a21f59bdac414c8a8206c
Simon <29654835+absolutelysimon@users.noreply.github.com>
2024-03-30 11:42:27 -0600

    Kinda working

--- src/components/EventDetailHeader/EventDetailHeader.tsx
+++ src/components/EventDetailHeader/EventDetailHeader.tsx
@@ -122,7 +122,7 @@ export default function EventDetailHeader({
                 className="inline-flex items-center rounded-md border border-transparent bg-gray-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
                 onClick={() => setIsOpen(true)}
               >
-                New Project
+                Add Project
               </button>
               {user?.role === "ADMIN" && (
                 <button
