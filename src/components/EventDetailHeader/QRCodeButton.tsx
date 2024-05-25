4a9f17b58133aaae05377400670c324a39de8edd
Yousef Fatouraee <fatouraee96@gmail.com>
2024-05-25 14:36:02 -0600

    Add QRCodeButton component

---
+++ src/components/EventDetailHeader/QRCodeButton.tsx
@@ -0,0 +1,11 @@
+import { useEffect, Fragment, useState, useRef } from "react";
+import QRCode from "qrcode";
+import { Dialog, Transition } from "@headlessui/react";
+
+type Props = {
+  link: string;
+};
+
+const QRCodeButton: React.FC<Props> = (props) => {};
+
+export default QRCodeButton;
